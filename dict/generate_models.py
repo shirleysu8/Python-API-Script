ed0eca4c48c348f225160eac4506511c4e182d00
Jennifer <wangxuanqing@hotmail.com>
2020-08-03 19:58:47 -0700

    modified generate_models.py

--- dict/generate_models.py
+++ dict/generate_models.py
@@ -4,13 +4,29 @@ from pathlib import Path
 import re
 import pdb
 
-def generate_paths(folder_name):
-    for path in Path('text').rglob('*.onnx'):
+model_list = []
+dict = {}   # ex: "vgg" : "https://github.com/onnx/models/blob/master/vision/classification/vgg/model/vgg16-7.onnx?raw=true"
+folder_names = ["text", "vision"]
+
+# generate paths to text or vision onnx models
+for model_type in folder_names:
+    generate_paths(model_type)
+            
+# print out the keys and values from dict 
+for keys,values in dict.items():
+    print (f'"{keys}"' + " : " + f'"{values}"' + " ,")
+
+def generate_paths(folder_names):
+    """
+    Generates a list of onnx models and
+    stores them in `dict` and `model_list`
+    """
+    for path in Path(folder_names).rglob('*.onnx'):
         model_list.append(str(path))
         if path != None:
-            # generate the url to download onnx models
+            # Generate onnx model URL
             url_paths = 'https://github.com/onnx/models/blob/master/' + str(path)  + '?raw=true'
-            # use regular expression to obtain model name
+            # Obtain model name
             pattern = re.compile(".*/([^/]+\\.*)/model")
             m = pattern.match(url_paths)
             if m != None:
@@ -20,18 +36,4 @@ def generate_paths(folder_name):
                     pattern = re.compile(".*/([^/]+\\.*).onnx")
                     m = pattern.match(url_paths)
                     file_name = m.group(1)
-                # add model and url to dict
                 dict[file_name] = url_paths
-
-# identify list of onnx models in model Zoo
-model_list = []
-dict = {}
-folder_name = ["text", "vision"]
-
-# generate paths to text or vision onnx models
-for model_type in folder_name:
-    generate_paths(model_type)
-            
-# print out the keys and values from dict 
-for keys,values in dict.items():
-    print (f'"{keys}"' + " : " + f'"{values}"' + " ,")
