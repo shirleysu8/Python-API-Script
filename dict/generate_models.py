908c8563ae64a7254dffb0310a642043629c6184
shirleysu8 <31262389+shirleysu8@users.noreply.github.com>
2020-08-06 13:59:49 -0700

    update generate model method

--- dict/generate_models.py
+++ dict/generate_models.py
@@ -6,15 +6,7 @@ import pdb
 
 model_list = []
 dict = {}   # ex: "vgg" : "https://github.com/onnx/models/blob/master/vision/classification/vgg/model/vgg16-7.onnx?raw=true"
-folder_names = ["text", "vision"]
-
-# generate paths to text or vision onnx models
-for model_type in folder_names:
-    generate_paths(model_type)
-            
-# print out the keys and values from dict 
-for keys,values in dict.items():
-    print (f'"{keys}"' + " : " + f'"{values}"' + " ,")
+folder_names = ["text", "vision"]            
 
 def generate_paths(folder_names):
     """
@@ -27,13 +19,16 @@ def generate_paths(folder_names):
             # Generate onnx model URL
             url_paths = 'https://github.com/onnx/models/blob/master/' + str(path)  + '?raw=true'
             # Obtain model name
-            pattern = re.compile(".*/([^/]+\\.*)/model")
+            pattern = re.compile(".*/([^/]+\\.*).onnx")
             m = pattern.match(url_paths)
             if m != None:
                 file_name = m.group(1)
-                # if model name is already in dict, then generate its name based on the file name
-                if file_name in dict:
-                    pattern = re.compile(".*/([^/]+\\.*).onnx")
-                    m = pattern.match(url_paths)
-                    file_name = m.group(1)
                 dict[file_name] = url_paths
+                
+# generate paths to text or vision onnx models
+for model_type in folder_names:
+    generate_paths(model_type)
+    
+# print out the keys and values from dict 
+for keys,values in dict.items():
+    print (f'"{keys}"' + " : " + f'"{values}"' + " ,")
